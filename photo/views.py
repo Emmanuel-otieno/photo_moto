525b09ea24251ccdb4ec1cfa1a3172381e085474
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-09 21:54:30 +0300

    add a search view function

--- photo/views.py
+++ photo/views.py
@@ -14,3 +14,15 @@ def image_location(request, location):
     images = Image.filter_by_location(location)
     print(images)
     return render(request, 'picha/place.html', {'location_images': images})
+
+
+ def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'pic/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'pic/search_results.html', {"message": message})   
