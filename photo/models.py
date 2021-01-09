9be2035b974e152e952de191a77cfcdb5a6852db
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-09 14:03:03 +0300

    add a image as a class in module

--- photo/models.py
+++ photo/models.py
@@ -15,3 +15,14 @@ class Category(models.Model):
     def delete_category(self):
         self.delete()
 
+
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    location = models.ForeignKey('Location',on_delete=models.CASCADE)
