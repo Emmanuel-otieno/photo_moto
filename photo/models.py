f98ef291b7efd7cb6d76bfd8be271e09da5a3a68
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-09 14:05:40 +0300

    add a image as a class in module

--- photo/models.py
+++ photo/models.py
@@ -26,3 +26,26 @@ class Image(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
     location = models.ForeignKey('Location',on_delete=models.CASCADE)
+
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
