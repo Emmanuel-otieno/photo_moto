70933f8e1332eb36eedaa3d7a34d5ffac2b3a84b
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-09 14:44:54 +0300

    update my view function

--- photo/views.py
+++ photo/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image,Location,Category
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to the Photo app')
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'picha/index.html', {'images': images[::-1], 'locations': locations})
+
 
