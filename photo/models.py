ca5b64d26f9863fcdb6adaee44cc67f1fa904b7d
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-09 21:20:59 +0300

    add methods that defines image modules

--- photo/models.py
+++ photo/models.py
@@ -28,6 +28,39 @@ class Image(models.Model):
     location = models.ForeignKey('Location',on_delete=models.CASCADE)
 
 
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['date']
+
+
+   
 
 class Location(models.Model):
     name = models.CharField(max_length=60)
