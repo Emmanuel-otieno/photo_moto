0b5850be3b222df558baf4d6df6db6424be32ee4
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-10 17:00:55 +0300

    add a media url

--- gallery/settings.py
+++ gallery/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photo.apps.PhotoConfig',
+    'photo',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 STATICFILE_DIRS =[
     os.path.join(BASE_DIR,"static"),
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
