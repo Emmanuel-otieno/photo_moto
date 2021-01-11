dbb44a04ce7c2298af8e55feacc2e14223a7c9f7
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-11 21:49:55 +0300

    edit allow host

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY='bhlv&93#h8fq4p5d*ctx*r!#x8l%bk_q8koqhbjv@fel$_#g88'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['photomoto.herokuapp.com','127.0.0.1']
 
 
 # Application definition
