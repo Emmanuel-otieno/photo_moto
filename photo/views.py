f292019896e3602cfd27e50b6ed5cdfe518050d0
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-11 11:48:04 +0300

    refresh my requirement file

--- photo/views.py
+++ photo/views.py
@@ -7,7 +7,7 @@ def index(request):
     images = Image.objects.all()
     locations = Location.get_locations()
     print(locations)
-    return render(request, 'pic/index.html', {'images': images[::-1], 'locations': locations})
+    return render(request, 'index.html', {'images': images[::-1], 'locations': locations})
 
 
 def image_location(request, location):
