3b6d611b46eafa72357fb7b1015c71c47beaf989
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-11 21:40:12 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -88,9 +87,7 @@ DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql',
            'NAME': 'pics',
-           'USER': 'moringa',
-           'PASSWORD': '1234',
-          
+        
        }
        
    }
