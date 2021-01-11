b097fa927de1c1a370a872f78d6fc2fa0271fe6c
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-11 16:13:57 +0300

    DEPLOYMENT

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY='bhlv&93#h8fq4p5d*ctx*r!#x8l%bk_q8koqhbjv@fel$_#g88'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
